ff90b1f7c6f47210283d25b2c78d372705594887
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-09-19 20:52:46 +0300

    add isInside and struct

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
-#include<iostream>
+#include <iostream>
+
+#include <pthread.h>
+
+struct Args
+{
+  double r_;
+  std::size_t tests_;
+  std::size_t seed_;
+  std::size_t result_;
+  std::exception_ptr eptr_;
+};
+
+bool isInside(double x, double y, double r)
+{
+  return (x * x + y * y) <= (r * r);
+}
+
 int main()
 {
-  
+  return 0;
 }
